93e59aedb49134c99161b87d4fdc76d627f94f3c
SOME Fiarma Landry <SOME Fiarma Landry@fiarmasome@gmail.com>
2024-09-19 09:01:36 +0000

    Insertion des donnees dans postgresql

---
+++ Schoolarship/databases/postgres_create_tables.sql
@@ -0,0 +1,21 @@
+
+
+CREATE TABLE IF NOT EXISTS opportunities_etudes (
+    id SERIAL PRIMARY KEY,
+    pays VARCHAR(255) NOT NULL,
+    titre TEXT NOT NULL,
+    type VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    niveau VARCHAR(255) NOT NULL,
+    financement TEXT NOT NULL,
+    date_limite TEXT,
+    conditions TEXT NOT NULL,
+    nombre_de_bourses TEXT NOT NULL,
+    domaine_concerne TEXT NOT NULL,
+    duree_d_etude TEXT NOT NULL,
+    pays_eligibles TEXT NOT NULL,
+    
+    
+
+    CONSTRAINT  unique_combination UNIQUE (pays, titre)
+);
